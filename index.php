c08f14cedff750d7781345e0f3ced9b6e605279e
TheMaximeSan <mxm.tournier@gmail.com>
2024-07-29 15:35:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,50 @@
 
 <main>
 
-    <p><?= $env_titre ?></p>
+    <div class="intro">
+        <div class="intro-title">
+            <p>Nos Labs <i class="ri-arrow-right-s-line"></i></p>
+        </div>
+    </div>
+
+
 
 </main>
+
+<style>
+
+    main{
+        border-top: solid 2px #0036DE;
+        border-bottom: solid 2px #0036DE;
+    }
+
+    .intro{
+        height:45px;
+        width: 100%;
+        border-bottom: solid 2px #0036DE;
+    }
+
+    .intro-title{
+        width: 50%;
+        border-right: solid 2px #0036DE;
+        height: 100%;
+    }
+
+    .intro-title p{
+        padding-top: 5px;
+        display: flex;
+        margin: 0 0 0 25%;
+        font-size: 22px;
+    }
+
+    .intro-title i {
+        margin-left: 10px;
+        font-size: 22px;
+    }
+
+
+
+</style>
 
 
 <?php $js_exemple(); ?>
