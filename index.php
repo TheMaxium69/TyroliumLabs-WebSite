0dc2210b661113c8723b10f0807c73801d3eb4b2
TheMaximeSan <mxm.tournier@gmail.com>
2024-07-29 16:01:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,37 @@
         </div>
     </div>
 
-    <p><?= $env_titre ?></p>
+    <div class="card-flex">
 
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+
+    </div>
 
 
 
@@ -53,6 +82,12 @@
         font-size: 22px;
     }
 
+    main .card-flex{
+        display: flex;
+        justify-content: space-around;
+        max-width: 1080px;
+        margin: 40px auto;
+    }
 
 
 </style>
