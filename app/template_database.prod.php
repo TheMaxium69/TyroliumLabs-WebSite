3575efc7fab14b61d84f3794f566fb3a4d8c5d45
TheMaximeSan <mxm.tournier@gmail.com>
2024-07-30 10:13:38 +0200

    update

---
+++ app/template_database.prod.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "";
+$userDB = "";
+$passDB = "";
+$Database = "";
+
+//MySQLi
+$ConnectDB = mysqli_connect($host, $userDB, $passDB, $Database);
+
+//PDO
+try{
+    $db = new PDO("mysql:host=" . $host . ";dbname=" . $Database, $userDB, $passDB);
+    $db->setAttribute(PDO::ERRMODE_EXCEPTION, 'ATTR_ERRMODE');
+}catch(PDOEXeption $e){
+    echo $e;
+}
+
+?>
