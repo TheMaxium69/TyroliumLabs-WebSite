787aefc1c937a6e7a6fab9e58d86323f9b463366
TheMaximeSan <mxm.tournier@gmail.com>
2024-07-30 12:03:20 +0200

    Create labs_test.php

---
+++ labs_test.php
@@ -0,0 +1,22 @@
+<?php $page_id = 2; require "@tyrositeframework/start.php"; ?>
+
+
+
+
+<header> <?php $cp_navbar(); ?> </header>
+
+<main>
+
+    <p><?= $env_titre ?></p>
+
+</main>
+
+
+<?php $js_exemple(); ?>
+
+
+
+
+
+
+<?php require "@tyrositeframework/end.php"; ?>
