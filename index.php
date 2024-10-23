530bb97368b2869bec2a49b266254986e0837f53
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-10-23 10:00:19 +0200

    update index
    "Nos labs" adjustments

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
     <?php $cp_hero() ?>
 
     <div id="titlemain">
-        <div>
-            <h1>
-                Nos labs <i class="ri-arrow-right-s-line"></i>
-            </h1>
+        <div class="container">
+            <div class="col-md-6">
+                <h1>
+                    Nos labs <i class="ri-arrow-right-s-line"></i>
+                </h1>
+            </div>
+            <div class="col-md-6"></div>
         </div>
-        <div></div>
-
     </div>
 
     <style>
@@ -29,24 +30,15 @@
             text-align: start;
         }
 
-        #titlemain div{
-            /*width: 10px;*/
-        }
-        #titlemain div:first-child{
-            border-right: 1px solid #0036DE;
-        }
-
-        #titlemain div:last-child{
-            border-left: 1px solid #0036DE;
-        }
 
         #titlemain h1{
             font-family: "Share Tech Mono", monospace;
             font-size: 24px;
             font-weight: 400;
-            margin: 15px 0;
+            margin: auto;
+            padding: 15px 0;
+            border-right: 2px solid #0036DE;
         }
-
 
     </style>
 
