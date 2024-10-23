7a582b26651a638c448793d5902aeb2ecf000b59
bthiebau <bastien.thiebaut@hotmail.com>
2024-10-23 12:10:04 +0200

    Update show.php

--- show.php
+++ show.php
@@ -8,10 +8,35 @@
 
     <?php $cp_heroshow() ?>
 
-    <?php $cp_moreshow() ?>
-    <?php $cp_useshow() ?>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-4 ">
+                <?php $cp_moreshow() ?>
+            </div>
+            <div class="offset-lg-2 col-lg-6">
+                <?php $cp_useshow() ?>
+            </div>
+        </div>
+    </div>
 
 
+    
+    <div id="paginationcadre">
+        <div class="" id="pagination">
+            <div class="col-lg-6">
+                <h1 id="prev">
+                <i class="ri-arrow-left-circle-line"></i>
+                    Page précédante
+                </h1>
+            </div>
+            <div class="col-lg-6">
+                <h1>
+                    Page suivante
+                    <i class="ri-arrow-right-circle-line"></i>
+                </h1>
+            </div>
+        </div>
+    </div>
 </main>
 
 
@@ -22,3 +47,30 @@
 
 
 <?php require "@tyrositeframework/end.php"; ?>
+
+<style>
+        #paginationcadre {
+            border-top: 2px solid #0036DE;
+        }
+        
+        #pagination{
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
+
+
+        #pagination h1{
+            font-family: "Share Tech Mono", monospace;
+            font-size: 24px;
+            font-weight: 400;
+            margin: auto;
+            padding: 15px 0;
+
+        }
+
+        #pagination #prev {
+            border-right: 2px solid #0036DE;
+        }
+        
+</style>
