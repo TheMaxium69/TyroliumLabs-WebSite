31dde59caeab428fe344b80b4a98a121ace847b8
Adele <166720543+Adeleloff@users.noreply.github.com>
2024-10-24 09:47:14 +0200

    Update getOneLabs.php

--- api/function/getOneLabs.php
+++ api/function/getOneLabs.php
@@ -1,1 +1,12 @@
 <?php
+require_once './app/database.dev.php';
+function getOneLabs(int $labsId): Array {
+
+    $stmt = $GLOBALS['db']->prepare('SELECT labs.*, icone.pictureUrl AS icon_url, background.pictureUrl AS background_url FROM labs JOIN picture AS icone ON labs.iconeId = icone.id JOIN picture AS background ON labs.backgroundId = background.id WHERE labs.id = ?');
+    $stmt->execute([$labsId]);
+    $results = $stmt->fetch();
+    if(!$results) throw new Exception("Page non trouvé");
+    var_dump($results);
+    return $results;
+
+}
