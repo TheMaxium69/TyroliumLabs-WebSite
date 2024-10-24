a6dc5112a733e84f9fef486861bffbd6ea4a9749
TheMaxium69 <mxm.tournier@gmail.com>
2024-10-24 10:21:02 +0200

    Update getOneLabs.php

--- api/function/getOneLabs.php
+++ api/function/getOneLabs.php
@@ -1,12 +1,14 @@
 <?php
-require_once './app/database.dev.php';
-function getOneLabs(int $labsId): Array {
 
-    $stmt = $GLOBALS['db']->prepare('SELECT labs.*, icone.pictureUrl AS icon_url, background.pictureUrl AS background_url FROM labs JOIN picture AS icone ON labs.iconeId = icone.id JOIN picture AS background ON labs.backgroundId = background.id WHERE labs.id = ?');
+getOneLabs(1);
+
+function getOneLabs(int $labsId) {
+    require '../../app/database.dev.php';
+
+    $stmt = $db->prepare('SELECT labs.*, icone.pictureUrl AS icon_url, background.pictureUrl AS background_url FROM labs JOIN picture AS icone ON labs.iconeId = icone.id JOIN picture AS background ON labs.backgroundId = background.id WHERE labs.id = ?');
     $stmt->execute([$labsId]);
     $results = $stmt->fetch();
-    if(!$results) throw new Exception("Page non trouvé");
-    var_dump($results);
+//    var_dump($results);
     return $results;
 
 }
