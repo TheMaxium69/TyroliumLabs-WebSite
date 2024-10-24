e3f57f6ead30870568cae4d44e6b6890f3b79199
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-10-24 09:47:45 +0200

    getAllLabs

--- api/function/getAllLabs.php
+++ api/function/getAllLabs.php
@@ -1,1 +1,22 @@
 <?php
+
+function getAllLabs() {
+    require 'app/database.dev.php';
+
+    $query = $db->query('SELECT * FROM labs');
+    $labs = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($labs as $lab) {
+        $stmt = $db->prepare('SELECT pictureUrl FROM pictures WHERE id = :id');
+        $stmt->execute(['id' => $lab['id']]);
+        $picture = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $lab['iconeUrl'] = $picture['pictureUrl'];
+        $lab['backgroundUrl'] = $picture['pictureUrl'];
+    }
+
+    var_dump($labs);
+        
+    return $labs;
+
+}
