3d088739d2d770646ac88c3f17995904d8a71b73
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-10-24 10:20:33 +0200

    Update getAll

--- api/function/getAllLabs.php
+++ api/function/getAllLabs.php
@@ -7,16 +7,25 @@ function getAllLabs() {
     $labs = $query->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($labs as $lab) {
-        $stmt = $db->prepare('SELECT pictureUrl FROM pictures WHERE id = :id');
-        $stmt->execute(['id' => $lab['id']]);
-        $picture = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $db->prepare('SELECT pictureUrl FROM picture WHERE id = :id');
 
-        $lab['iconeUrl'] = $picture['pictureUrl'];
-        $lab['backgroundUrl'] = $picture['pictureUrl'];
+        $stmt->execute(['id' => $lab['iconeId']]);
+        $iconeUrl = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $stmt->execute(['id' => $lab['backgroundId']]);
+        $backgroundUrl = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        var_dump($iconeUrl);
+        var_dump($backgroundUrl);
+
+        $lab['iconeUrl'] = $iconeUrl['pictureUrl'];
+        $lab['backgroundUrl'] = $backgroundUrl['pictureUrl'];
+
+        var_dump($lab);
     }
 
     var_dump($labs);
-        
     return $labs;
-
 }
+
+getAllLabs();
