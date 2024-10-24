fd7c4074fd2b836fa75c642bfd085cd2e06ae3af
bthiebau <bastien.thiebaut@hotmail.com>
2024-10-24 09:54:06 +0200

    Update addLike.php

--- api/function/addLike.php
+++ api/function/addLike.php
@@ -1,6 +1,6 @@
 <?php
 function addLike (int $id, string $ip){
-    require __DIR__.'app\database.dev.php' ;
+    require '../../app/database.dev.php';
 
     $preparedQuery = $db->prepare("INSERT INTO `like_labs` (`idLabs`, `ip`) VALUES (?, ?)");
     $preparedQuery->execute([$id, $ip]);
